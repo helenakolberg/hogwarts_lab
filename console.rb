1635135be8a39f8750ca9ba5e8e752c27f6388bd
Helena <inger.helena@gmail.com>
2020-07-08 15:53:37 +0100

    created more instances of student class

--- console.rb
+++ console.rb
@@ -11,6 +11,22 @@ student1 = Student.new({
 })
 student1.save()
 
+student2 = Student.new({
+    'first_name' => 'Hermione',
+    'last_name' => 'Granger',
+    'house' => 'Gryffindor',
+    'age' => "13"
+})
+student2.save()
+
+student3 = Student.new({
+    'first_name' => 'Draco',
+    'last_name' => 'Malfoy',
+    'house' => 'Slytherin',
+    'age' => "14"
+})
+student3.save()
+
 
 
 
